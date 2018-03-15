d790f7318e53eab2b35c6c0ce60616abb689f6a7
dollarb1llz <xd0llarb1llzx@gmail.com>
2018-03-15 14:32:13 -0500

    first commit

---
+++ res/includes/header.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+  <title>Ideas</title>
+  <link href="/../img/fav.ico" rel="shortcut icon" />
+  <link href="/../css/reset.css" type="text/css" rel="stylesheet" />
+  <link href="/../css/style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+
+  <header>
+    <nav>
+      <ul>
+        <li><a href="./index.php">Ideas</a></li>
+        <!-- <li><a href="./family.html">Family</a></li>
+        <li><a href="./content.html">Content</a></li> -->
+        <li><a href="./plan.php">Plan</a></li>
+      </ul>
+    </nav>
+  </header>
